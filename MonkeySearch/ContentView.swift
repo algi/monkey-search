2c81307d8ca339edf209103f38fbbbbc6b6807cc
Marian Boucek <boucekm@gmail.com>
2019-08-28 22:11:51 +0100

    Adds basic table for browser

--- MonkeySearch/ContentView.swift
+++ MonkeySearch/ContentView.swift
@@ -6,11 +6,39 @@
 //  Copyright © 2019 Marian Bouček. All rights reserved.
 //
 
+import CoreData
 import SwiftUI
 
 struct ContentView: View {
+
+    @Environment(\.managedObjectContext) var viewContext: NSManagedObjectContext
+    @FetchRequest(fetchRequest: fetchRequest()) var data: FetchedResults<Estate>
+
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List {
+                HStack {
+                    Text("Property Name")
+                    Spacer()
+                    Text("£400 pw")
+                        .foregroundColor(.gray)
+                }
+            }
+            .navigationBarTitle("Browser")
+            .navigationBarItems(trailing: Button("Reload") {
+                // fire event, where background fetch operation will be performed
+                print("Reload not yet implemented.")
+            })
+            .navigationViewStyle(DoubleColumnNavigationViewStyle())
+        }
+    }
+
+    static func fetchRequest() -> NSFetchRequest<Estate> {
+        let request: NSFetchRequest<Estate> = Estate.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(keyPath: \Estate.name, ascending: true)]
+        request.predicate = NSPredicate(format: "status == %@", "New");
+
+        return request
     }
 }
 
