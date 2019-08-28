737efcb0352e8196d3fa27d6255abed79e15bcfd
Marian Boucek <boucekm@gmail.com>
2019-08-28 21:31:16 +0100

    Initial Commit

---
+++ MonkeySearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MonkeySearch
+//
+//  Created by Marian Bouček on 28/08/2019.
+//  Copyright © 2019 Marian Bouček. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
