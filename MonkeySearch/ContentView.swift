4367bc95f7995879a18321b8463416d2def95d49
Marian Boucek <boucekm@gmail.com>
2019-09-04 16:25:49 +0200

    Cleanup in ContentView

--- MonkeySearch/ContentView.swift
+++ MonkeySearch/ContentView.swift
@@ -6,15 +6,10 @@
 //  Copyright © 2019 Marian Bouček. All rights reserved.
 //
 
-//import CoreData
 import SwiftUI
-
-// hmm... 🤔
-//extension Estate: Identifiable {}
 
 struct ContentView: View {
 
-//    @FetchRequest(fetchRequest: fetchRequest()) var data: FetchedResults<Estate>
     @State private var data = [EstateRecord]()
 
     var body: some View {
@@ -33,52 +28,11 @@ struct ContentView: View {
             }
             .navigationBarTitle("Browser")
             .navigationBarItems(trailing: Button("Reload") {
-                fetchNewData { (records) in
-                    self.data = records
-                }
+                // TODO: fetch new data
             })
             .navigationViewStyle(DoubleColumnNavigationViewStyle())
         }
     }
-}
-
-//private func fetchRequest() -> NSFetchRequest<Estate> {
-//    let request: NSFetchRequest<Estate> = Estate.fetchRequest()
-//    request.sortDescriptors = [NSSortDescriptor(keyPath: \Estate.name, ascending: true)]
-//    request.predicate = NSPredicate(format: "status == %@", "New");
-//
-//    return request
-//}
-
-private func fetchNewData(completionHandler: @escaping ([EstateRecord]) -> Void) {
-    guard let fetchURL = URL(string: "https://www.foxtons.co.uk/properties-to-rent/muswell-hill-n10/?price_to=500&bedrooms_from=2&expand=2") else {
-        print("Unable to parse fetch URL for Foxtons.")
-        return
-    }
-
-    let task = URLSession.shared.downloadTask(with: fetchURL) { (location, response, error) in
-        if let error = error {
-            print("Unable to fetch data, reason: \(error.localizedDescription)")
-            return
-        }
-
-        guard let location = location else {
-            print("Unable to find location with downloaded data.")
-            return
-        }
-
-        do {
-            let html = try String(contentsOf: location)
-            let result = try FoxtonsParser().parse(html)
-
-            // TODO: map to entities, etc.
-            completionHandler(result)
-        }
-        catch {
-            print("Unable to parse HTML document, reason: \(error.localizedDescription)")
-        }
-    }
-    task.resume()
 }
 
 struct ContentView_Previews: PreviewProvider {
