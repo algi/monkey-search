187b68f2a856d12c032f36f1962317838067d2f0
Marian Boucek <boucekm@gmail.com>
2019-09-06 16:22:59 +0100

    Make code a slightly more pretty

--- MonkeySearch/AppDelegate.swift
+++ MonkeySearch/AppDelegate.swift
@@ -27,11 +27,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let container = NSPersistentContainer(name: "MonkeySearch")
         container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
 
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        container.loadPersistentStores { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
-        })
+        }
+
         return container
     }()
 }
