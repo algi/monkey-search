3642fafd18ab8cb45a3cd7dabcf9885725acbf64
Marian Boucek <boucekm@gmail.com>
2019-09-12 12:41:08 +0100

    Removes unnecessary TODO

--- MonkeySearch/Parser/FoxtonsParser.swift
+++ MonkeySearch/Parser/FoxtonsParser.swift
@@ -24,7 +24,7 @@ class FoxtonsParser: NSObject, AgencyParser {
                 let propertyName = element.at_xpath("./div/h6")?.text,
                 let detailLink = element.at_xpath("./div/h6/a/@href")?.text,
                 let detailURL = URL(string: "\(FoxtonsParser.baseURL)\(detailLink)"),
-                let price = Double(element.at_xpath("./div/h2/strong/data")?.text ?? ""), // TODO: Double
+                let price = Double(element.at_xpath("./div/h2/strong/data")?.text ?? ""),
                 let description = element.at_xpath("./div/p[@class='description']")?.text,
                 let previewImageURL = URL(string: findPreviewImageURL(in: element) ?? "")
             else {
